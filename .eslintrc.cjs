361f8300b14fbe23975d11cc35ae7c0178d7aca3
K0rtz <johnmecnx12@gmail.com>
2022-12-24 18:47:23 +0100

    first commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,26 @@
+import React, { version } from 'react';
+
+module.exports = {
+	env: {
+		browser: true,
+		es2021: true
+	},
+	settings: {
+		react: {
+			version: 'detect'
+		}
+	},
+	extends: [
+		'plugin:react/recommended',
+		'plugin:react/jsx-runtime',
+		'standard',
+		'eslint-config-prettier'
+	],
+	overrides: [],
+	parserOptions: {
+		ecmaVersion: 'latest',
+		sourceType: 'module'
+	},
+	plugins: ['react'],
+	rules: {}
+};
---
+++ src/main.jsx
@@ -0,0 +1,7 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+
+const app = <h1>Vite app</h1>;
+const container = document.getElementById('root');
+
+ReactDOM.render(app, container);
